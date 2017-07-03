6adfe6911540aec1b1b3793a040e431ad1dfeb22
nfosci <nathan@animatedchild.com>
2017-07-02 21:40:36 -0400

    Update input-fetcher-dropdown.js

--- js/input-fetcher-dropdown.js
+++ js/input-fetcher-dropdown.js
@@ -1,3 +1,28 @@
+/*
+LICENSE
+
+The MIT License
+
+Copyright (c) 2016, nfosci
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 (function ( $ ) {
     $.fn.inputFetcherDropdown = function( options ) {
@@ -28,7 +53,6 @@
 			thisObject.val( thisisit.children('.'+ settings.dataObjectSelection).text() );
 			event.stopPropagation();
 			$(settings.storeDataIdObject).val( thisisit.attr('id') );
-			alert( settings.storeDataIdObject + " -- " + thisisit.attr('id'));
 			
 			//$(settings.dropdownObject).html(''); 
 			$(settings.dropdownObject).hide();
