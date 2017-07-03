e7b07c39aa8934b12b6632481fac54497e15db69
nfosci <nathan@animatedchild.com>
2017-07-02 20:58:03 -0400

    Update input-fetcher-dropdown.js

--- js/input-fetcher-dropdown.js
+++ js/input-fetcher-dropdown.js
@@ -3,13 +3,16 @@
     $.fn.inputFetcherDropdown = function( options ) {
         var thisObject = this;
 		
+		this.parent().append("<div id='"+ thisObject.attr('id') +"-input-fetcher-dropdown-result' class='input-fetcher-dropdown-result'></div>");
+		this.parent().append("<input type='hidden' id='"+ thisObject.attr('id') +"-selected-index' name='"+ thisObject.attr('id') +"-selected-result' >");
+		
 		// setup default options, also allow overwriting of defaults with options
         var settings = $.extend({
 			
-            dropdownObject: "#input-fetcher-dropdown-result" ,
+            dropdownObject: "#" + thisObject.attr('id') + "-input-fetcher-dropdown-result" ,
 			dataSourceURL: "data/test-data.json",
 			dataIndex: "uuid",
-			storeDataIdObject: "#input-fetcher-dropdown-selected-id",
+			storeDataIdObject: "#" + thisObject.attr('id') + "-selected-index", 
 			displayFormat: "%%image%% %%name%%<br>%%birthday%% - %%contact_name%% - %%contact_number%%",
 			displayData: {'image':'TRUE', 'name':'TRUE', 'birthday':'TRUE', 'contact_name':'TRUE', 'contact_number':'FALSE'},
 			dataObjectSelection: 'name'
@@ -25,8 +28,9 @@
 			thisObject.val( thisisit.children('.'+ settings.dataObjectSelection).text() );
 			event.stopPropagation();
 			$(settings.storeDataIdObject).val( thisisit.attr('id') );
+			alert( settings.storeDataIdObject + " -- " + thisisit.attr('id'));
 			
-			$(settings.dropdownObject).html(''); 
+			//$(settings.dropdownObject).html(''); 
 			$(settings.dropdownObject).hide();
 			
 		});
@@ -65,16 +69,16 @@
 						$.each( result_json, function( i, val ) {
 							var htmlout = settings.displayFormat;
   			
-							$(settings.dropdownObject).append( '<div id=\'' + val[settings.dataIndex] + '\' ></div>' );
+							$(settings.dropdownObject).append( '<div id=\''+settings.dataIndex+'-' + val[settings.dataIndex] + '\' ></div>' );
 						
-							$('#'+val[settings.dataIndex]).addClass( 'input-fetcher-dropdown-show' );
+							$('#'+settings.dataIndex+'-'+val[settings.dataIndex]).addClass( 'input-fetcher-dropdown-show' );
 							if(index == result_json.length-1)
 							{
-								$('#'+val[settings.dataIndex]).addClass( 'last' );
+								$('#'+settings.dataIndex+'-'+val[settings.dataIndex]).addClass( 'last' );
 							}
-							$('#'+val[settings.dataIndex]).addClass( 'input-fetcher-dropdown-option' );
+							$('#'+settings.dataIndex+'-'+val[settings.dataIndex]).addClass( 'input-fetcher-dropdown-option' );
 							 
-							$('#'+val[settings.dataIndex]).attr( 'align', 'left' );
+							$('#'+settings.dataIndex+'-'+val[settings.dataIndex]).attr( 'align', 'left' );
 		
 							$.each(settings.displayData, function( x, y ) {
 								if(val[x] == null) 
@@ -94,7 +98,7 @@
 									htmlout = htmlout.replace('%%' + x + '%%', "<span class='"+x+" input-fetcher-dropdown-hidden'>" + val[x] + "</span>");
 								}
 							});
-							$('#'+val[settings.dataIndex]).html(htmlout);
+							$('#'+settings.dataIndex+'-'+val[settings.dataIndex]).html(htmlout);
 							index++;
   						});	
 				    }
@@ -103,13 +107,3 @@
 		});
     };
 }( jQuery ));
-
-
-
-
-
-
-
-
-
-
